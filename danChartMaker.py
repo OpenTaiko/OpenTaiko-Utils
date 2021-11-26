b810ad92c7cfbb0028960c86056601d2f9b26994
0aubsq <0aubsq@gmail.com>
2021-11-26 09:52:54 +0100

    Extract difficulties (dan chart maker)

--- danChartMaker.py
+++ danChartMaker.py
@@ -6,6 +6,7 @@ import codecs
 from enum import Enum
 import sys
 import os
+import re
 
 def eprint(*args, **kwargs): print(*args, file=sys.stderr, **kwargs)
 
@@ -48,6 +49,66 @@ class Exam:
 		self.configuration = config
 		for i in range(songC):
 			self.Parts.append(ExamPart())
+			
+DifficultyDict = {
+	"oni":3,
+	"edit":4,
+	"ura":4,
+	"hard":2,
+	"normal":1,
+	"easy":0,
+	"kantan":0,
+	"futsuu":1,
+	"muzukashii":2,
+	"extreme":3,
+	"0":0,
+	"1":1,
+	"2":2,
+	"3":3,
+	"4":4
+}
+
+class Difficulty:
+	"""Chart difficulty informations"""
+	body = ""
+	starRating = 0
+	balloon = ""
+	
+	def __init__(self, contents):
+		# Star rating
+		srpattern = re.compile(r"^LEVEL:(\S+)", re.MULTILINE)
+		self.starRating = re.findall(srpattern, contents)[0]
+		
+		# Balloon
+		bpattern = re.compile(r"^BALLOON:(\S+)", re.MULTILINE)
+		self.balloon = re.findall(bpattern, contents)[0]
+		
+		# Body
+		bdpattern = re.compile(r"^#START((?:.|\n(?!#END))*)", re.MULTILINE)
+		self.body = re.findall(bdpattern, contents)[0]
+			
+class Chart:
+	"""Chart general informations"""
+	difficulties = [None, None, None, None, None]
+	title = ""
+	subtitle = ""
+	demoStart = 0
+	bpm = 140
+	wave = ""
+	
+	def __init__(self, contents):
+		self.raw = contents
+		
+	def partition(self):
+		pattern = re.compile(r"(?:^TITLE:|^COURSE:)(?:.|\n(?!COURSE))*", re.MULTILINE)
+		matchlist = re.findall(pattern, self.raw)
+		
+		header = matchlist.pop(0)
+		
+		for e in matchlist:
+			diffpattern = re.compile(r"^COURSE:(\S+)", re.MULTILINE)
+			diff = DifficultyDict[re.findall(diffpattern, e)[0].lower()]
+			self.difficulties[diff] = Difficulty(e)
 
 
 if __name__ == "__main__":
@@ -69,7 +130,11 @@ if __name__ == "__main__":
 		eprint(f'// Enter the chart {i + 1} file name : ')
 		fileName = input()
 		with open(os.path.join(sys.path[0], fileName), "r", encoding="shift-jis") as f:
-			charts.append(f.read())
+			charts.append(Chart(f.read()))
+			
+	# Chart preprocessing
+	for c in charts:
+		c.partition()
 	
 	# Exam general informations
 	exams = []
@@ -84,5 +149,5 @@ if __name__ == "__main__":
 			exams.append(Exam(1, songCount))
 	
 	
-	print(charts[0])
+	#print(charts[0].raw)
 	
